01edcf4967bd6f261b8a76673cb12ab8b9c2637e
Marytiluli <marytiluli@gmail.com>
2025-04-13 19:30:08 +0300

    Gets the image inside the lightbox

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Opens the lightbox and displays the clicked image
 function openLightbox(element) {
     const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
 }
