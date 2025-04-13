7be9f074fcabe19364172ff970dcd7b8fea32fb4
Marytiluli <marytiluli@gmail.com>
2025-04-13 19:31:17 +0300

    Adds 'visible' class to show the lightbox

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ function openLightbox(element) {
     const lightbox = document.getElementById('lightbox');
     const lightboxImg = document.getElementById('lightbox-img');
     lightboxImg.src = element.src;
+    lightbox.classList.add('visible');
 }
